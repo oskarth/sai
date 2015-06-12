e1a445742630b5b7a188ce794477e0e8c3aedab3
oskarth <me@oskarth.com>
2015-06-13 01:03:47 +0200

    fmt

--- evaluator.rkt
+++ evaluator.rkt
@@ -3,17 +3,12 @@
 ; Programming is debugging. 100% REPL all the time.
 ; Don't copy paste. it leads to stupid bugs.
 
-;(require scheme/mpair)
-
-; Alternatively, call (define apply-builtin apply) before defining our apply.
 (require (only-in racket/base [apply apply-builtin]))
 
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
         ((quoted? exp) (text-of-quotation exp))
-;        ((assignment? exp) (eval-assignment exp env))
-;        ((definition? exp) (eval-definition exp env))
         ((lambda? exp) (make-procedure (lambda-parameters exp)
                                        (lambda-body exp)
                                        env))
@@ -24,13 +19,20 @@
 
 (define (apply proc args)
   (cond ((primitive-procedure? proc) (apply-primitive-procedure proc args))
+        ((compound-procedure? proc)
+         (eval-sequence
+           (procedure-body proc)
+           (extend-environment (procedure-parameters proc)
+                               args
+                               (procedure-environment proc))))
         (else (error "Unknown procedure type -- APPLY" proc))))
 
-(define (variable? exp) (symbol? exp))
-(define (application? exp) (pair? exp)) ; later in cond-statement
-(define (self-evaluating? exp) (or (number? exp) (string? exp)))
-(define (tagged-list? exp tag) (and (pair? exp) (eq? (car exp) tag)))
-
+(define (variable? exp)             (symbol? exp))
+(define (application? exp)          (pair? exp)) ; later in cond-statement
+(define (self-evaluating? exp)      (or (number? exp) (string? exp)))
+(define (tagged-list? exp tag)      (and (pair? exp) (eq? (car exp) tag)))
+(define (last-exp? seq)             (null? (cdr seq)))
+(define (no-operands? ops)          (null? ops))
 (define (quoted? exp)               (tagged-list? exp 'quote))
 (define (assignment? exp)           (tagged-list? exp 'set!))
 (define (definition? exp)           (tagged-list? exp 'define))
@@ -41,23 +43,30 @@
 (define (primitive-procedure? proc) (tagged-list? proc 'primitive))
 (define (compound-procedure? proc)  (tagged-list? proc 'procedure))
 
-; operations on environments
-;
-; this is an env, first-frame and enclosing environment
-; (cons (cons (mcons 'a (mcons 'b '())) (mcons 1 (mcons 2 '()))) '())
-; (define e0 (extend-environment (list 'a 'b) (list 1 2) the-empty-environment))
-;
-(define (enclosing-environment env)    (cdr env))
-(define (first-frame env)             (car env))
-(define the-empty-environment         '())
-(define (make-frame variables values) (cons variables values))
-(define (frame-variables frame)       (car frame))
-(define (frame-values frame)          (cdr frame))
+(define (first-exp seq)                 (car seq))
+(define (rest-exps seq)                 (cdr seq))
+(define (enclosing-environment env)     (cdr env))
+(define (first-frame env)               (car env))
+(define (frame-variables frame)         (car frame))
+(define (frame-values frame)            (cdr frame))
+(define (text-of-quotation exp)         (cadr exp))
+(define (assignment-variable exp)       (cadr exp))
+(define (assignment-value exp)          (caddr exp))
+(define (lambda-parameters exp)         (cadr exp))
+(define (lambda-body exp)               (cddr exp))
+(define (operator exp)                  (car exp))
+(define (operands exp)                  (cdr exp))
+(define (first-operand ops)             (car ops))
+(define (rest-operands ops)             (cdr ops))
+(define (procedure-parameters p)        (cadr p))
+(define (procedure-body p)              (caddr p))
+(define (procedure-environment p)       (cadddr p))
+(define (primitive-implementation proc) (cadr proc))
 
-; this is the problem, so we comment it out!
-;(define (add-binding-to-frame! var val frame)
-;  (set-car! frame (cons var (car frame)))
-;  (set-cdr! frame (cons val (cdr frame))))
+(define the-empty-environment                '())
+(define (make-lambda parameters body)        (cons 'lambda (cons parameters body)))
+(define (make-frame variables values)        (cons variables values))
+(define (make-procedure parameters body env) (list 'procedure parameters body env))
 
 (define (extend-environment vars vals base-env)
   (if (= (length vars) (length vals))
@@ -78,61 +87,10 @@
         (scan (frame-variables frame) (frame-values frame)))))
   (env-loop env))
 
-;(define (set-variable-value! var val env)
-;  (define (env-loop env)
-;    (define (scan vars vals)
-;      (cond ((null? vals) (env-loop (enclosing-environment env)))
-;            ((eq? var (car vars)) (set-car! vals val))
-;            (else (scan (cdr vars) (cdr vals)))))
-;    (if (eq? env the-empty-environment)
-;      (error "Unbound variable -- SET!" var)
-;      (let ((frame (first-frame env)))
-;        (scan (frame-variables frame) (frame-values frame)))))
-;  (env-loop env))
-
-;(define (define-variable! var val env)
-;  (let ((frame (first-frame env)))
-;    (define (scan vars vals)
-;      (cond ((null? vars) (add-binding-to-frame! var val frame))
-;            ((eq? var (car vars)) (set-car! vals val))
-;            (else (scan (cdr vars) (cdr vals)))))
-;    (scan (frame-variables frame) (frame-values frame))))
-;
-(define (text-of-quotation exp) (cadr exp))
-(define (assignment-variable exp) (cadr exp))
-(define (assignment-value exp) (caddr exp))
-
-;(define (definition-variable exp)
-;  (if (symbol? (cadr exp))
-;    (cadr exp)
-;    (caadr exp)))
-;
-;(define (definition-value exp)
-;  (if (symbol? (cadr exp))
-;    (caddr exp)
-;    (make-lambda (cdadr exp)   ; formal parameters
-;                 (cddr exp)))) ; body
-
-(define (lambda-parameters exp) (cadr exp))
-(define (lambda-body exp) (cddr exp))
-(define (make-lambda parameters body) (cons 'lambda (cons parameters body)))
-
-;(define (eval-assignment exp env)
-;  (set-variable-value! (assignment-variable exp)
-;                       (eval (assignment-value exp) env)
-;                       env)
-;  'ok)
-
-;(define (eval-definition exp env)
-;  (define-variable! (definition-variable exp)
-;                    (eval (definition-value exp) env)
-;                    env))
-
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
-(define (no-operands? ops) (null? ops))
-(define (first-operand ops) (car ops))
-(define (rest-operands ops) (cdr ops))
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first exps) env)
+              (eval-sequence (rest-exps exps) env))))
 
 (define (list-of-values exps env)
   (if (no-operands? exps)
@@ -140,34 +98,20 @@
     (cons (eval (first-operand exps) env)
           (list-of-values (rest-operands exps) env))))
 
-(define (make-procedure parameters body env) (list 'procedure parameters body env))
-(define (procedure-parameters p) (cadr p))
-(define (procedure-body p) (caddr p))
-(define (procedure-environment p) (cadddr p))
-
-(define (primitive-implementation proc) (cadr proc))
-
 (define (apply-primitive-procedure proc args)
   (apply-builtin (primitive-implementation proc) args))
 
-; do we not need +?
 (define primitive-procedures
   (list (list 'car car)
          (list 'cdr cdr)
          (list 'cons cons)
          (list 'null? null?)
-         (list '+ +)
-         ;; more
-         ))
-
-; this is gettting seriously annoying, surel we can coerce at just a few points?
-; not everything is mutable, just...the environment :<
-(define (mcadr x) (car (cdr x)))
+         (list '+ +)))
 
 (define (primitive-procedure-names) (map car primitive-procedures))
 
 (define (primitive-procedure-objects)
-  (map (lambda (proc) (list 'primitive (mcadr proc)))
+  (map (lambda (proc) (list 'primitive (cadr proc)))
        primitive-procedures))
 
 (define (setup-environment)
@@ -175,15 +119,12 @@
           (extend-environment (primitive-procedure-names)
                               (primitive-procedure-objects)
                               the-empty-environment)))
-;    (define-variable! 'true true initial-env)
-;    (define-variable! 'false false initial-env)
     initial-env))
 
 (define the-global-environment (setup-environment))
 
 (define input-prompt ";;; Eval input:")
 (define output-prompt ";;; Eval input:")
-
 (define (prompt-for-input string) (newline) (newline) (display string) (newline))
 (define (announce-output string) (newline) (display string) (newline))
 
@@ -205,15 +146,67 @@
 
 
 
+; NYI
 
+; in eval:
+;        ((assignment? exp) (eval-assignment exp env))
+;        ((definition? exp) (eval-definition exp env))
 
+; mutable cons cells etc, setup-env
+;    (define-variable! 'true true initial-env)
+;    (define-variable! 'false false initial-env)
+
+;(define (add-binding-to-frame! var val frame)
+;  (set-car! frame (cons var (car frame)))
+;  (set-cdr! frame (cons val (cdr frame))))
+
+;(define (eval-assignment exp env)
+;  (set-variable-value! (assignment-variable exp)
+;                       (eval (assignment-value exp) env)
+;                       env)
+;  'ok)
+
+;(define (eval-definition exp env)
+;  (define-variable! (definition-variable exp)
+;                    (eval (definition-value exp) env)
+;                    env))
+
+;(define (set-variable-value! var val env)
+;  (define (env-loop env)
+;    (define (scan vars vals)
+;      (cond ((null? vals) (env-loop (enclosing-environment env)))
+;            ((eq? var (car vars)) (set-car! vals val))
+;            (else (scan (cdr vars) (cdr vals)))))
+;    (if (eq? env the-empty-environment)
+;      (error "Unbound variable -- SET!" var)
+;      (let ((frame (first-frame env)))
+;        (scan (frame-variables frame) (frame-values frame)))))
+;  (env-loop env))
+
+;(define (define-variable! var val env)
+;  (let ((frame (first-frame env)))
+;    (define (scan vars vals)
+;      (cond ((null? vars) (add-binding-to-frame! var val frame))
+;            ((eq? var (car vars)) (set-car! vals val))
+;            (else (scan (cdr vars) (cdr vals)))))
+;    (scan (frame-variables frame) (frame-values frame))))
+;
+
+;(define (definition-variable exp)
+;  (if (symbol? (cadr exp))
+;    (cadr exp)
+;    (caadr exp)))
+;
+;(define (definition-value exp)
+;  (if (symbol? (cadr exp))
+;    (caddr exp)
+;    (make-lambda (cdadr exp)   ; formal parameters
+;                 (cddr exp)))) ; body
+;
 
 ;eval-assignment exp env
 ;eval-definition exp env
 ;eval-if exp env
-;make-procedure foo bar baz
-;lambda-parameters exp
-;lambda-body exp
 ;eval-sequence foo bar
 ;begin-actions exp
 ;cond->if exp
